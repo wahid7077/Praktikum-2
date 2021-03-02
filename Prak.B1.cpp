d4cae0a0a32629148cb9b1f11427b8f727902636
wahid7077 <72840089+wahid7077@users.noreply.github.com>
2021-03-03 01:58:05 +0700

    Add files via upload

---
+++ Prak.B1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "PROGRAM ARRAY MEMBALIK HURUF" << endl;
+	cout << "============================" << endl;
+	char a[6] = {'p', 'e', 'n', 's', 'i', 't'};
+	int i, j;
+	
+	cout << "--Kata sebelum dibalik : --"<< endl;
+	for (i=0; i<6; i++){
+		cout << a [i];	
+	}
+	cout << "\n\n";
+	cout << "--kata setelah dibalik : --"<< endl;
+	for (j=6; j>=0; j--){
+		cout << a [j];
+	}
+	return 0;
+}
