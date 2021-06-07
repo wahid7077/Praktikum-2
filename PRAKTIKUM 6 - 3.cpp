dd0641962de32fb3afb65a8a82127ed3723f640c
wahid7077 <72840089+wahid7077@users.noreply.github.com>
2021-06-08 02:46:24 +0700

    Add files via upload

---
+++ PRAKTIKUM 6 - 3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void array(int [],int,int);
+int main(){
+ int a[]={0,1,2,3,4,5,6,7,8};
+ int x, i=8;
+ printf("WAHID AMIN SAMSUDIN\n");
+ printf("20051397077\n");
+ printf("Input x : ");scanf("%d", &x);
+ array(a,x,i);
+}
+void array(int a[],int x,int i){
+ if(i<0){
+ puts("x bukan merupakan anggota array a");
+ return;
+ }
+ if(x==a[i]){
+ puts("x merupakan anggota array a");
+ return;
+ }
+ array(a,x,i-1);
+}
