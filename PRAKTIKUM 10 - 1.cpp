9d47bad81758bc4b4c5c66f55d38bb50c1fe0af0
wahid7077 <72840089+wahid7077@users.noreply.github.com>
2021-06-08 02:52:02 +0700

    Add files via upload

---
+++ PRAKTIKUM 10 - 1.cpp
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main()
+
+printf("WAHID AMIN SAMSUDIN\n");
+ printf("20051397077\n");
+cout<<"\nData yang ingin dicari : ";
+cin>>c;
+i=0;
+posisi=0;
+while(i<19 && A[i]!=c){
+ i++;
+}
+if (A[i]!=c){
+ cout<<"Maaf data yang dicari tidak ada";
+}else if(posisi=i+1)
+   cout<<"ditemukan pada posisi ke "<<posisi;
+getch();
+}
